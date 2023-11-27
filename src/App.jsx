426c42345bc5e926c96a02f14cd6bf0b6c4d7fe4
David Linconl <linconldavid1@hotmail.com>
2023-11-27 15:34:08 -0300

    Finished EventStd

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       {/* <ArrayStd /> */}
 
       {/*Estudo Eventos  */}
-      <EventsStd />
+      {/* <EventsStd /> */}
     </main>
   );
 };
--- src/components/EventsStd.jsx
+++ src/components/EventsStd.jsx
@@ -2,13 +2,22 @@ import React from "react";
 
 const EventsStd = () => {
   const [text, setText] = React.useState("");
+
   const handle = (event) => {
     const textoo = event.target.getAttribute("text");
     setText(textoo);
   };
 
+  // Add eventos em Elementos como Body e Html.Aí temos que usar com JS puro.
+  let num = 0;
+  function handleScroll() {
+    num = num + 1;
+    console.log(num);
+  }
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <div>
+    <div style={{ height: "950px" }}>
       <p>{text}</p>
       <button onClick={handle} text="Este botão pode ser acionado">
         click
